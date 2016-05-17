60ca92660c567eabf6d8b6acf7f177bd7ffd3fb0
Sam <TheSkeltch@gmail.com>
2016-05-17 16:57:11 -0400

    added loginCheck

---
+++ loginCheck.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Script used to login from the mainscreenactivity
+Created and debugged by Samuel Cheung
+*/
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+//Checking credentials
+
+if(isset($_POST['id'])){
+	
+	require_once __DIR__ . '/database_handler.php';
+	
+	$db = new database_handler();
+	
+	$checkQuery = "SELECT username FROM USERS where id = $id";
+	
+	if(!$checkStmt = $db->con->prepare($checkQuery)){
+		echo "Prepare failed: (" . $db->con->errno . ")" . $db->con->error;
+	}
+	$checkStmt->execute();
+	$checkStmt->bind_result($username);
+	$checkStmt->fetch();
+	$checkStmt->close();
+	if($username==""){
+		echo "failed";
+	}
+	else{
+		echo "success";
+	}
+	//echo $username;
+}
+?>
