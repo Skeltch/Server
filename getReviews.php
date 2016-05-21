d0869188a0b54cad9981c636ed38a7df95d7c93f
Sam <TheSkeltch@gmail.com>
2016-05-20 21:10:11 -0400

    comma bug fix

--- getReviews.php
+++ getReviews.php
@@ -6,7 +6,7 @@ Created and debugged by Samuel Cheung
 	require_once __DIR__ . '/database_handler.php';
 	$db = new database_handler();
 	$id = $_POST['id'];
-	$reviewQuery = "SELECT * FROM REVIEW WHERE tutorID = $id ORDER BY commends DESC, date DESC LIMIT ? ?";
+	$reviewQuery = "SELECT * FROM REVIEW WHERE tutorID = $id ORDER BY commends DESC, date DESC LIMIT ?, ?";
 	if(!$reviewStmt = $db->con->prepare($reviewQuery)){
 		echo "Prepare failed: (" . $db->con->errno . ")" . $db->con->error;
 	}
