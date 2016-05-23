8fcf03c994f741c16cbbcf391fef7b0c77491d09
Sam <TheSkeltch@gmail.com>
2016-05-23 00:52:28 -0400

    Ability to remove price

--- editProfile.php
+++ editProfile.php
@@ -109,7 +109,7 @@ Created and debugged by Samuel Cheung
 			mysqli_query($db->con, "UPDATE USERS SET graduation_year=NULL WHERE id = $id");
 		}
 		if($price=="NULL"){
-			mysqli_query($db->con, "UPDATE USERS SET graduation_year=NULL WHERE id = $id");
+			mysqli_query($db->con, "UPDATE USERS SET price=NULL WHERE id = $id");
 		}
 		echo "success";
 		exit;
